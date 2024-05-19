afc5636bdaf396b9bb655efeda30c3a456f62a4e
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-05-19 23:04:07 +0800

    chore: Update environment variables and package dependencies
    This commit updates the `.env` file with new database credentials and JWT information. It also adds the necessary dependencies for working with PostgreSQL and Sequelize in the `package.json` file.

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({path: `${process.cwd()}/.env`})
+
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
